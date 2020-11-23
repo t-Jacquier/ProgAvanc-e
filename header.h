c1abe03ac254bccc64f3ef7f6c75000c86f781e0
Timéo Jacquier <timeo.jacquier1@etu.univ-lorraine.fr>
2020-11-23 16:57:27 +0100

    header avec les dimensions de l'écran

---
+++ header.h
@@ -0,0 +1,19 @@
+//
+// Created by timeo on 23/11/2020.
+//
+
+#ifndef PROJET_HEADER_H
+#define PROJET_HEADER_H
+
+
+/**
+ * \brief Largeur de l'écran de jeu
+ */
+#define SCREEN_WIDTH 600
+
+/**
+ * \brief Hauteur de l'écran de jeu
+ */
+#define SCREEN_HEIGHT 600
+
+#endif //PROJET_HEADER_H
