4d66828f0436b2cc3ef5cab47b56185778ccf4ef
Simon Jolivalt <simon.jolivalt6@etu.univ-lorraine.fr>
2021-01-02 11:24:41 +0100

    fonctions du module platform

--- platform.c
+++ platform.c
@@ -17,4 +17,35 @@ platform_t initP(int x, int y, int w, int h, bool est_visible){
     return platform;
 }
 
-    
+platform_t* tabP(int nb_platform){
+    platform_t *p= malloc(nb_platform*sizeof(platform_t));
+    int x = 0;
+    srand(time(NULL));
+    for (int i = 0; i < nb_platform; i++){
+        x = (rand() % 600) + 480;
+        p[i] = initP(x, 435, 48, 31, true);
+    }
+    return p;
+}
+
+void freePlatform(platform_t* p[]){
+    free(p[0]);
+}
+
+void copyEnnemies(SDL_Renderer* screen, SDL_Texture* text, platform_t tab[], int nb_platform){
+    for (int i = 0; i < nb_platform; i++){
+        if (tab[i].visible){
+                SDL_RenderCopy(screen, text, NULL, &tab[i].coo);
+        }
+    }
+}
+
+void movePlatformRight(platform_t* p, int nb_platform){
+  for (int i = 0; i < nb_platform; i++)
+    p[i].coo.x -= 10;
+}
+
+void movePlatformLeft(platform_t* p, int nb_platform){
+  for (int i = 0; i < nb_platform; i++)
+    p[i].coo.x += 10;
+}
--- platform.h
+++ platform.h
@@ -6,11 +6,23 @@
 #include "header.h"
 
 
+
 struct s_platform{
     SDL_Rect coo; 
     bool visible; 
 };
 
+
 typedef struct s_platform platform_t;
 
 platform_t initP(int x, int y, int w, int h, bool est_visible);
+
+platform_t* tabP(int nb_platform);
+
+void freePlatform(platform_t* p[]);
+
+void copyEnnemies(SDL_Renderer* screen, SDL_Texture* text, platform_t tab[], int nb_platform);
+
+void movePlatformRight(platform_t* p, int nb_platform);
+
+void movePlatformLeft(platform_t* en, int nb_platform);
